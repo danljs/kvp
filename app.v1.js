1e18c8b51275014c0cf19acc971c0dd9b183fd36
Daniel Liu <daniel@eieihome.com>
2016-04-13 10:51:33 -0400

    combine v and c

--- app.v1.js
+++ app.v1.js
@@ -57,156 +57,126 @@ M.prototype.data = function(){
 }
 //================================================================
 function $(id){return document.getElementById(id)}
-var V = function(model){
-	this._model = model;
+var V = function(){
+	this._model = new M();
+	$("kv-input").addEventListener("keypress", this.on_keypress.bind(this));
+	$("add-button").addEventListener("click", this.on_click_add.bind(this));
+	$("order-value-button").addEventListener("click", this.on_click_order_value.bind(this,true));
+	$("order-key-button").addEventListener("click", this.on_click_order_key.bind(this,false));
+	$("delete-button").addEventListener("click", this.on_click_delete.bind(this));
+	$("show-xml").addEventListener("click", this.on_click_show_xml.bind(this,true));
+	$("show-list").addEventListener("click", this.on_click_show_list.bind(this,false));
+	$("kv-list").addEventListener("change", this.on_click_kv_list.bind(this));
+	$("load-json").addEventListener("click", this.on_click_load_json.bind(this));
+	$("save-json").addEventListener("click", this.on_click_save_json.bind(this));
 }
 V.prototype.add = function (key,value) {
 	$("kv-list").options.add(new Option(key + '=' + value,key)) ;
 	$("kv-input").value = "" ;
 }
-V.prototype.remove_kv = function () {
-	var kv_list = $("kv-list");
-    for(var i= kv_list.options.length-1; i >= 0; i--) {
-        kv_list.options[i].selected?kv_list.remove(i):"" ;
-    }
-}
+
 V.prototype.reload_kv = function (arr) {
 	var kv_list = $("kv-list") ;
 	while (kv_list.options.length > 0) {kv_list.remove(0)}
 	arr.map(function(c){kv_list.options.add(new Option(c.key + '=' + c.value,c.key))}) ;
 }
-V.prototype.addListener = function(listeners){
-	listeners.map(function(c){
-		$(c.id).addEventListener(c.event,c.listener);
-	})
-}
-V.prototype.get_new_kv=function(){
-	$("message").innerHTML="";
-	return $("kv-input").value;
-}
-V.prototype.set_error=function(){
-	$("message").innerHTML="Invalid key/value pair";
-}
+
 V.prototype.show_list=function(status){
 	$("kv-xml").style.display = status?"none":"block" ;
 	$("kv-list").style.display = status?"block":"none" ;
 	$("show-xml").style.display = status?"block":"none" ;
 	$("show-list").style.display = status?"none":"block" ;
 }
-V.prototype.set_xml=function(kv_xml){
-	$("kv-xml").value=kv_xml;
+
+V.prototype.on_keypress=function(e){
+	e.keyCode === 13?this.on_click_add():"";
 }
-//================================================================
-var C = function(){
-	var _model = new M();
-    var _view = new V(_model);
 
-    _view.addListener([
-    	{id:"kv-input", event:"keypress", listener:on_keypress},
-    	{id:"add-button", event:"click", listener:on_click_add},
-    	{id:"order-value-button", event:"click", listener:on_click_order_value},
-    	{id:"order-key-button", event:"click", listener:on_click_order_key},
-    	{id:"delete-button", event:"click", listener:on_click_delete},
-    	{id:"show-xml" ,event:"click", listener:on_click_show_xml},
-    	{id:"show-list" ,event:"click", listener:on_click_show_list},
-    	{id:"kv-list" ,event:"change", listener:on_click_kv_list},
-    	{id:"load-json" ,event:"click", listener:on_click_load_json},
-    	{id:"save-json" ,event:"click", listener:on_click_save_json}
-    ]);
+V.prototype.on_click_add=function(e){
+	$("message").innerHTML="";
+	var kv = $("kv-input").value;
 
-    function on_keypress(e){
-		e.keyCode === 13?on_click_add():"";
+	var patt0 = /\w+\s*=\s*\w+/g;
+	var patt1 = /(^[a-zA-Z0-9\s*]*$)/g;
+	if(patt0.test(kv) && patt1.test(kv.replace('=',''))){
+		kv = kv.split('=');
+		kv[0] = kv[0].trim();
+		kv[1] = kv[1].trim();
+		this.add(kv[0],kv[1]);
+		this._model.add(kv[0],kv[1]);
+	}else{
+		$("message").innerHTML="Invalid key/value pair";
 	}
+}
+V.prototype.on_click_order_value=function(e){
+	this.reload_kv(this._model.sort_value())
+}
+V.prototype.on_click_order_key=function(e){
+	this.reload_kv(this._model.sort_key())
+}
+V.prototype.on_click_delete=function(e){
+	this._model.remove()
+	var kv_list = $("kv-list");
+    for(var i= kv_list.options.length-1; i >= 0; i--) {
+        kv_list.options[i].selected?kv_list.remove(i):"" ;
+    }
+}
+V.prototype.on_click_show_xml=function(e){
+	$("kv-xml").value=this._model.xml();
+	this.show_list(false)
+}
+V.prototype.on_click_show_list=function(e){
+	this.show_list(true)
+}
+V.prototype.on_click_kv_list=function(e){
+	this._model.set_selected(e.srcElement.selectedIndex)
+}
 
-	function on_click_add(e){
-		var kv = _view.get_new_kv();
-		load_kv(kv);
-	}
-	function on_click_order_value(e){
-		_view.reload_kv(_model.sort_value())
-	}
-	function on_click_order_key(e){
-		_view.reload_kv(_model.sort_key())
-	}
-	function on_click_delete(e){
-		_model.remove()
-		_view.remove_kv();
-	}
-	function on_click_show_xml(e){
-		_view.set_xml(_model.xml())
-		_view.show_list(false)
-	}
-	function on_click_show_list(e){
-		_view.show_list(true)
-	}
-	function on_click_kv_list(e){
-		_model.set_selected(e.srcElement.selectedIndex)
-	}
-	function load_kv(kv){
-		if(check(kv)){
-			kv = kv.split('=');
-			kv[0] = kv[0].trim();
-			kv[1] = kv[1].trim();
-			add_kv(kv[0], kv[1])
-		}else{
-			_view.set_error();
-		}
-	}
-
-	function add_kv(k,v){
-		_view.add(k,v);
-		_model.add(k,v);
-	}
-
-	function check(new_kv){
-		var patt0 = /\w+\s*=\s*\w+/g;
-		var patt1 = /(^[a-zA-Z0-9\s*]*$)/g;
-		return patt0.test(new_kv) && patt1.test(new_kv.replace('=',''))
-	}
-	function on_click_load_json(e){
-		new Promise(function(resolve, reject){
-			var x = document.createElement("INPUT");
-		    x.setAttribute("type", "file");
-		    document.body.appendChild(x);
-		    x.style = "visibility:hidden";
-		    x.addEventListener('change',resolve);
-		    x.click();
-		    document.body.removeChild(x);
-		})
-		.then(
-			function (e){
-				return new Promise(function(resolve, reject){
-			    	var file = e.target.files[0];
-					if (!file) {return;}
-					var reader = new FileReader();
-					reader.onload = resolve;
-					reader.readAsText(file);
-				})
-	    	},
-	    	function(){
-				console.log('Something wrong...');
-			}
-		)
-		.then(
-			function (e){
-				JSON.parse(e.target.result).map(function(e,i){
-					add_kv(e.key,e.value)
-				});
-			},
-			function(){
-				console.log('Something wrong...');
-			}
-		)
-	}
-	function on_click_save_json(e){
-	    var x = document.createElement("a");    
-	    x.href = 'data:text/json;charset=utf-8,' + JSON.stringify(_model.data());
-	    x.style = "visibility:hidden";
-	    x.download = "kv.json";
+V.prototype.on_click_load_json=function(e){
+	var me = this;
+	new Promise(function(resolve, reject){
+		var x = document.createElement("INPUT");
+	    x.setAttribute("type", "file");
 	    document.body.appendChild(x);
+	    x.style = "visibility:hidden";
+	    x.addEventListener('change',resolve);
 	    x.click();
 	    document.body.removeChild(x);
-	}
+	})
+	.then(
+		function (e){
+			return new Promise(function(resolve, reject){
+		    	var file = e.target.files[0];
+				if (!file) {return;}
+				var reader = new FileReader();
+				reader.onload = resolve;
+				reader.readAsText(file);
+			})
+    	},
+    	function(){
+			console.log('Something wrong...');
+		}
+	)
+	.then(
+		function (e){
+			JSON.parse(e.target.result).map(function(e,i){
+				me.add(e.key,e.value);
+				me._model.add(e.key,e.value);
+			});
+		},
+		function(){
+			console.log('Something wrong...');
+		}
+	)
+}
+V.prototype.on_click_save_json=function(e){
+    var x = document.createElement("a");    
+    x.href = 'data:text/json;charset=utf-8,' + JSON.stringify(this._model.data());
+    x.style = "visibility:hidden";
+    x.download = "kv.json";
+    document.body.appendChild(x);
+    x.click();
+    document.body.removeChild(x);
 }
 //================================================================
-window.onload = function(){new C();};
+window.onload = new V();
