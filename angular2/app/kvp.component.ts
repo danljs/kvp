bb8e24ab3e0901e7be3a1bf2d88d92c7270eb6a9
danljs <daniel.liu1973@gmail.com>
2016-09-25 19:50:31 -0400

    roughly finished

--- angular2/app/kvp.component.ts
+++ angular2/app/kvp.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 
 export class KvPair {
-  key: string;
-  value: string;
+  key: string
+  value: string
 }
-const KVS: KvPair[] = [
-	  { key: '11', value: 'Mr. Nice' },
-	  { key: '12', value: 'Narco' },
-	  { key: '13', value: 'Bombasto' },
-	  { key: '20', value: 'Tornado' }
-	];
+
 @Component({
   selector: 'kvp-app',
   template: `
@@ -17,8 +12,8 @@ const KVS: KvPair[] = [
 		<div class="header">
 			<div class="label">Key/Value Pair</div>
 			<div class="row">
-				<div class="left-area"><input id="kv-input" type="text"/></div>
-				<div class="right-area"><button id="add-button">Add</button></div>
+				<div class="left-area"><input id="kv-input" type="text" [(ngModel)]="newKv" (keypress)="onEnter($event)"/></div>
+				<div class="right-area"><button id="add-button" (click)="onAdd(newKv)">Add</button></div>
 			</div>
 			<div class="row" id="message">{{message}}</div>
 		</div>
@@ -27,27 +22,124 @@ const KVS: KvPair[] = [
 			<div class="row label">Key/Value Pair List</div>
 			<div class="row">
 				<div class="left-area">
-					<select id="kv-list" size="10">
-						<option *ngFor="let kv of kvs">{{kv.key}}</option>
+					<select id="kv-list" size="10" *ngIf="toggle_list">
+						<option *ngFor="let kv of kvs; let i = index" (click)="onSelect(kv,i)">{{kv.key}}={{kv.value}}</option>
 					</select>
-					<textarea readOnly id="kv-xml">{{kv_xml}}</textarea>
+					<textarea readOnly id="kv-xml" *ngIf="!toggle_list">{{kv_xml}}</textarea>
 				</div>
 				<div class="right-area">
-					<div class="row"><button id="order-value-button">OrderByValue</button></div>
-					<div class="row"><button id="order-key-button">OrderByKey</button></div>
-					<div class="row"><button id="delete-button">Delete</button></div>
-					<div class="row"><button id="show-xml">ShowXML</button></div>
-					<div class="row"><button id="show-list">ShowList</button></div>
-					<div class="row"><button id="load-json">LoadData</button></div>
-					<div class="row"><button id="save-json">SaveData</button></div>
+					<div class="row"><button id="order-value-button" (click)="order_value()">OrderByValue</button></div>
+					<div class="row"><button id="order-key-button" (click)="order_key()">OrderByKey</button></div>
+					<div class="row"><button id="delete-button" (click)="delete()">Delete</button></div>
+					<div class="row"><button id="show-xml" *ngIf="toggle_list" (click)="show_xml()">ShowXML</button></div>
+					<div class="row"><button id="show-list" *ngIf="!toggle_list" (click)="show_list()">ShowList</button></div>
+					<div class="row"><button id="load-json" (click)="load_json()">LoadData</button></div>
+					<div class="row"><button id="save-json" (click)="save_json()">SaveData</button></div>
 				</div>
 			</div>
 		</div>
 	</div>
   `
 })
-export class AppComponent { 
+export class AppComponent {
+	selectedKv: KvPair
+
+	selectedIndex = -1
 	message = ''
 	kv_xml = ''
-	kvs = KVS
+	kvs = []
+	newKv = ''
+	toggle_list = true
+
+	onSelect(kv: KvPair, index): void {
+	  this.selectedKv = kv
+	  this.selectedIndex = index
+	}
+	onEnter(e): void {
+		e.keyCode === 13 ? this.onAdd() : '';
+	}
+	onAdd(): void {
+		let patt0 = /\w+\s*=\s*\w+/g
+		let patt1 = /(^[a-zA-Z0-9\s*]*$)/g
+		let kv = this.newKv
+		if(patt0.test(kv) && patt1.test(kv.replace('=', ''))){
+			kv = kv.split('=')
+			kv[0] = kv[0].trim()
+			kv[1] = kv[1].trim()
+			this.kvs.push({key: kv[0], value: kv[1]});
+			this.newKv = '';
+		}else{
+			this.message = 'Invalid key/value pair';
+		}
+	}
+
+	delete(): void {
+		this.kvs.splice(this.selectedIndex, 1)
+	}
+
+	order_value(): void {
+		this.kvs.sort((a, b) => a.key < b.key ? -1 : (a.key > b.key ? 1 : 0))
+	}
+	order_key(): void {
+		this.kvs.sort((a, b) => a.value < b.value ? -1 : (a.value > b.value ? 1 : 0))
+	}
+
+	show_xml(): void {
+		
+		var kv_xml = "<!DOCTYPE html>\n";
+		kv_xml += "<html>\n<body>\n";
+		kv_xml += "<select id='kv-list' size='10'>\n";
+		this.kvs.map(function(c, i){
+      kv_xml += "<option value='" + c.key + "'" ;
+	    // kv_xml += selected === i ? " selected" : "" ;
+	    kv_xml += ">" + c.key + '=' + c.value + "</option>\n";
+		});
+    kv_xml += "</select>";
+    kv_xml += "\n</body>\n</html>\n";
+
+    this.kv_xml = kv_xml
+		this.toggle_list = false 
+	}
+
+	show_list(): void {
+		this.toggle_list = true 
+	}
+
+	load_json(): void {
+		new Promise((resolve, reject) => {
+			let x = document.createElement('INPUT')
+		    x.setAttribute('type', 'file')
+		    document.body.appendChild(x)
+		    x.style = 'visibility:hidden'
+		    x.addEventListener('change', resolve)
+		    x.click()
+		    document.body.removeChild(x)
+		})
+		.then(
+			e => new Promise((resolve, reject) => {
+		    let file = e.target.files[0];
+				if(!file){return;}
+				let reader = new FileReader();
+				reader.onload = resolve;
+				reader.readAsText(file);
+			}),
+    	() => console.log('Something wrong...')
+		)
+		.then(
+			e => {
+				this.kvs = this.kvs.concat(JSON.parse(e.target.result))
+			},
+			() => console.log('Something wrong...')
+		)
+	}
+
+	save_json(): void {
+		let x = document.createElement('a');    
+    x.href = 'data:text/json;charset=utf-8,' + JSON.stringify(this.kvs);
+    x.style = 'visibility:hidden';
+    x.download = 'kv.json';
+    document.body.appendChild(x);
+    x.click();
+    document.body.removeChild(x);
+	};
 }
