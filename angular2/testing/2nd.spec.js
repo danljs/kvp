24a91574fef0039e56055e0046b2be7287eaf8db
Daniel Liu <daniel.liu1973@gmail.com>
2016-10-14 22:49:54 -0400

    The first test case

---
+++ angular2/testing/2nd.spec.js
@@ -0,0 +1,4 @@
+describe('1st tests', function () {
+    it('false is false', function () { return expect(false).toBe(false); });
+});
+//# sourceMappingURL=2nd.spec.js.map
---
+++ angular2/testing/2nd.spec.ts
@@ -0,0 +1,3 @@
+describe('1st tests', () => {
+  it('false is false', () => expect(false).toBe(false));
+});
